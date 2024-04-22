e21df361b3cc83b749b099acc2c3c945ba951b6c
irene <wunder.frau@yandex.by>
2024-04-22 21:03:54 +0300

    validation: add function check_first_last_symb to validate the first and last symbols of each line in the map. Ensures all lines start and end with an obstacle for proper boundary definition.

--- so_long.h
+++ so_long.h
@@ -99,5 +99,6 @@ void		key_hook(mlx_key_data_t keydata, void *args);
 
 void	check_constrained(const t_span *data);
 void	check_frst_lst_lines(const t_span *data);
+void	check_frst_lst_symb(const t_span *data);
 
 # endif
